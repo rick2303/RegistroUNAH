de56b87016c744200c6034aae52be57f3e5bdaa1
Rick <76531295+rick2303@users.noreply.github.com>
2023-06-22 17:03:21 -0600

    Actualiazacion del Backend (#8)
    * Delete Frontend directory
    
    * Delete README.md
    
    * Delete Backend directory
    
    * Create README.md
    
    * Delete .gitattributes
    
    * Connection to server azure
    
    * Cambios
    
    * changes
    
    * Creación de registro y login
    
    ---------
    
    Co-authored-by: GelenAmador <98992768+GelenAmador@users.noreply.github.com>
    Co-authored-by: Gerardo2811 <gerarmontoya@yahoo.es>

--- Backend/src/routes/usuarios.routes.js
+++
@@ -1,9 +0,0 @@
-import {Router} from 'express';
-
-import {getUsuarios} from '../controllers/usuarios.controllers';
-
-const router = Router();
-
-router.get('/usuarios', getUsuarios);
-console.log('router funcionando')
-export default router;
