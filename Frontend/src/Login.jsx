a13db95015187b3542edb2448e4e194d649e7fe1
GelenAmador <98992768+GelenAmador@users.noreply.github.com>
2023-06-25 14:43:52 -0600

    Update Login.jsx

--- Frontend/src/Login.jsx
+++ Frontend/src/Login.jsx
@@ -1,64 +1,79 @@
-import { useState } from "react";
-import axios from "axios";
-import "./Login.css";
-import logo from "./img/UNAH_logo.png";
+import { useState } from "react"
+import "./Login.css"
+
 
 export function Login({ setUser }) {
-  const [Id, setIdId] = useState("");
-  const [contraseña, setContrasena] = useState("");
-  const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(false);
+    const [usuario, setUsuario] = useState("")
+    const [password, setPassword] = useState("")
+    const [error, setError] = useState(false)
+    const [loading, setLoading] = useState(false)
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+    const handleSubmit = (e) => {
+        e.preventDefault()
 
-    if ([Id, contraseña].includes("")) {
-      setError(true);
-      return;
+
+        if ([usuario, password].includes("")) {
+            setError(true)
+            return
+        }
+        setError(false)
+
+        setLoading(true)
+        setTimeout(() => {
+            setLoading(false)
+            setUser(usuario)
+        }, 2000)
+        data = {
+            usuario: usuario,
+            pass: password
+        }
+        fetch('http://localhost:5000/iswAdmin', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+            })
+            .then(response => {
+              // Manejar la respuesta del backend
+            })
+            .catch(error => {
+              // Manejar cualquier error que ocurra durante la solicitud
+            });
     }
-    setError(false);
-    setLoading(true);
+    
+    return (
+        <>
+            <form
+                className="formulario"
+                onSubmit={handleSubmit}
+                id="formularioEstilo"
+            >
+            <h3>Número de cuenta</h3>
+                <input
+                    type="text"
+                    value={usuario}
+                    onChange={(e) => setUsuario(e.target.value)}
+                />
+            <h3>Contraseña</h3>
+                <input
+                    type="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <p></p>
+                <button>Accerder</button>
+            </form>
+            <br></br>
+            {error && <p className="error">Debes completar todos los campos</p>}
 
-    // Send ID and password to the backend
-    axios
-      .post("http://localhost:5000/login", { Id, contraseña })
-      .then((response) => {
-        setLoading(false);
-        const responseData = response.data; // Obtener la respuesta del backend
-        window.location.href=responseData;
-      })
-      .catch((error) => {
-        setLoading(false);
-        // Handle error
-        // ...
-      });
-  };
+            {loading && <span class="loader"></span>}
+            
+            
+            <a href="/src/html/Administracion.html" >¿Olvidaste tu contraseña?</a>
 
-  return (
-    <>
-      <img src={logo} alt="Logo" />
-      <h1>UNAH Login</h1>
-      <form className="formulario" onSubmit={handleSubmit}>
-        <h2>Nº Cuenta</h2>
-        <input
-          type="text"
-          value={Id}
-          onChange={(e) => setIdId(e.target.value)}
-        />
-        <h2>Contraseña</h2>
-        <input
-          type="password"
-          value={contraseña}
-          onChange={(e) => setContrasena(e.target.value)}
-        />
-        <p></p>
-        <button>Acceder</button>
-      </form>
-      <br></br>
-      {error && <p className="error">Debes completar todos los campos</p>}
-
-      {loading && <span className="loader"></span>}
-      <a href="/src/html/Administracion.html">¿Olvidaste tu contraseña?</a>
-    </>
-  );
+        </>
+        
+    )
 }
+
+
