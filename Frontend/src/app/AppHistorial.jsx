db385fbc3af26134eebf28387cb1bd9d2041a059
nikolas-oss <nicolastislovomontenegro@gmail.com>
2023-06-30 17:00:57 -0600

    Agregando Historial

---
+++ Frontend/src/app/AppHistorial.jsx
@@ -0,0 +1,15 @@
+import '../App.css'
+import Navbar from "../components/Navbar.jsx";
+import MenuHistorial from "../components/MenuHistorial";
+
+function AppHistorial() {
+
+  return (
+    <>
+      <Navbar /> 
+      <MenuHistorial /> 
+    </>
+  )
+}
+
+export default AppHistorial
---
+++ Frontend/src/mainHistorial.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import AppHistorial from './app/AppHistorial.jsx' 
+import './CssAdministracion.css'
+
+
+
+ReactDOM.render(
+    <React.StrictMode>
+      <AppHistorial />
+    </React.StrictMode>,
+    document.getElementById('historial')
+  )
