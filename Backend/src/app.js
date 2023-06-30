9791bc3a5bfc01002e51b80ab39e9ea0bb072fc5
Gerardo2811 <gerarmontoya@yahoo.es>
2023-06-30 16:39:42 -0600

    Envio de JSON con informacion de usuarios y de sus perfiles

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,7 +3,8 @@ import config from './config';
 import empleadosRoute from './routes/empleados.routes'
 import usuariosRoute from "./routes/usuarios.routes";
 import loginRoute from './routes/login.routes'
-import redireccionamientoRoute from './routes/redireccionamientos.routes'
+
+import estudiantePerfil from './routes/estudiantePerfil.routes'
 //import {} from "./csv/csv";
 const app= express();
 const cors = require('cors');
@@ -25,6 +26,6 @@ app.use(express.urlencoded({ extended:false}));
 
 console.log(config.port);
 
-app.use(empleadosRoute,loginRoute,redireccionamientoRoute);
+app.use(empleadosRoute,loginRoute,estudiantePerfil);
 app.use(usuariosRoute);
 
--- Frontend/src/components/MenuEstudiante.jsx
+++ Frontend/src/components/MenuEstudiante.jsx
@@ -1,4 +1,5 @@
 //import React from "react";
+import React, { useEffect, useState } from "react";
 import { FcUpload } from "react-icons/fc";
 import { FcCalendar } from "react-icons/fc";
 import { FcBusinessman } from "react-icons/fc";
@@ -7,10 +8,21 @@ import "bootstrap/dist/css/bootstrap.min.css"
 
 
 function MenuEstudiante() {
+
+  const [nombreUsuario, setNombreUsuario] = useState("");
+
+  useEffect(() => {
+    const storedData = localStorage.getItem("userData");
+    if (storedData) {
+      const userData = JSON.parse(storedData);
+      const nombre = userData.Nombre; // Asegúrate de utilizar la clave correcta para el nombre del usuario
+      setNombreUsuario(nombre);
+    }
+  }, []);
   return (
     <section className="bg-white md:mt-5">
       <h1 className="text-2xl  text-center font-bold pt-2 text-gray-900 sm:text-3xl">
-       Estudiante
+       Bienvenido {nombreUsuario}
       </h1>
       <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-8 lg:px-8">
         <div className=" grid grid-cols-1 gap-4 sm:grid-cols-3 md:grid-cols-4 md:gap-8">
