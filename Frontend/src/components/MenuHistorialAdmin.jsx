a3444e8d48ab3eda662e88a8134c6a127781152d
GelenAmador <98992768+GelenAmador@users.noreply.github.com>
2023-07-03 20:56:15 -0600

    Update MenuHistorialAdmin.jsx

--- Frontend/src/components/MenuHistorialAdmin.jsx
+++ Frontend/src/components/MenuHistorialAdmin.jsx
@@ -1,106 +1,100 @@
-
-import "bootstrap/dist/css/bootstrap.min.css"
-import React,{useState,useEffect} from 'react';
-import DataTable from 'react-data-table-component';
-import 'styled-components'
-import { Input, Button } from 'reactstrap';
+import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useState, useEffect } from "react";
+import DataTable from "react-data-table-component";
+import "styled-components";
+import { Input, Button } from "reactstrap";
 
 const MenuHistorialAdmin = () => {
+  const [users, setUsers] = useState([]);
+  const [inputValue, setInputValue] = useState("");
 
+  const showData = async (NumCuenta) => {
+    const URL = "http://localhost:5000/historial";
+    const response = await fetch(URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ NumCuenta }),
+    });
+    const data = await response.json();
+    console.log(data);
+    setUsers(data);
+  };
 
-    const [users,setUsers] = useState([])
-    const [inputValue, setInputValue] = useState('');
-    
+  useEffect(() => {
+    showData(inputValue);
+  }, [inputValue]); // Ejecutar el efecto cada vez que inputValue cambie
 
-    //funcion para  mostrar los datos con fetch
-    const URL = 'URL SERVIDOR'
-    const showData = async () => {
-      const response = await fetch (URL) 
-      const data = await response.json()
-      console.log(data) 
-      setUsers(data)
-      setInputValue(data)
-    } 
+  const handleButtonClick = () => {
+    showData(inputValue);
+  };
 
-    useEffect ( ()=>{
-      showData()
-    }, [] )
+  //configuramos las columnas para DataTable
 
-    //configuramos las columnas para DataTable
+  const columnas = [
+    {
+      name: "CODIGO",
+      selector: (row) => row.CODIGO,
+    },
+    {
+      name: "ASIGNATURA",
+      selector: (row) => row.ASIGNATURA,
+    },
+    {
+      name: "UV",
+      selector: (row) => row.UV,
+    },
+    {
+      name: "SECCION",
+      selector: (row) => row.SECCION,
+    },
+    {
+      name: "AÑO",
+      selector: (row) => row.Año,
+    },
+    {
+      name: "PERIODO",
+      selector: (row) => row.PERIODO,
+    },
+    {
+      name: "CALIFICACION",
+      selector: (row) => row.CALIFIACION,
+    },
+    {
+      name: "OBS",
+      selector: (row) => row.OBS,
+    },
+  ];
 
-    const columnas = [
-      {
-        name:'ID',
-        selector : row => row.id
-      },
-      {
-        name:'CODIGO',
-        selector : row => row.asignatura
-      },
-      {
-        name:'ASIGNATURA',
-        selector : row => row.asignatura
-      },
-      {
-        name:'UV',
-        selector : row => row.uv
-      },
-      {
-        name:'SECCION',
-        selector : row => row.seccion
-      },
-      {
-        name:'Año',
-        selector : row => row.año
-      },
-      {
-        name:'PERIODO',
-        selector :row => row.periodo
-      },
-      {
-        name:'CALIFICACION',
-        selector :row => row.calificacion
-      },
-      {
-        name:'OBS',
-        selector : row => row.obs
-      },
-      {
-        name:'IdDocente',
-        selector : row => row.IdDocente
-      },
-     ]
-      
-    //Mostramos la data en DataTable
+  //Mostramos la data en DataTable
 
-     return (
-
-
-
-      <div className="App">
+  return (
+    <div className="App">
       <div>
-      <Input
-          style={{textAlign:"center", marginTop:"10px",marginLeft:"10px",maxWidth: '300px' }}
+        <Input
+          style={{
+            textAlign: "center",
+            marginTop: "20px",
+            marginLeft: "30px",
+            maxWidth: "300px",
+          }}
+          id="inputCuenta"
           type="text"
           value={inputValue}
           onChange={(e) => setInputValue(e.target.value)}
           placeholder="Ingresar Numero de Cuenta"
         />
       </div>
-      
-      <div style={{marginLeft:"10px", marginTop: '20px' }}>
-      <Button onClick={MenuHistorialAdmin} color="primary">Mostrar</Button>
+
+      <div style={{ marginLeft: "30px", marginTop: "20px" }}>
+        <Button onClick={handleButtonClick} color="primary">
+          Mostrar
+        </Button>
       </div>
-      <DataTable
-        columns={columnas}
-        data={users}
-        pagination
-        ></DataTable>
-      </div>   )
-      
-
-    
-  }
-
+      <DataTable columns={columnas} data={users} pagination></DataTable>
+    </div>
+  );
+};
 
 export default MenuHistorialAdmin;
