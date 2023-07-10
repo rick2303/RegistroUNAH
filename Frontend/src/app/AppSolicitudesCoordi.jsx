3c619e617cacd6cb395b09ba76f0b419c9a3468b
GelenAmador <98992768+GelenAmador@users.noreply.github.com>
2023-07-09 20:52:44 -0600

    Add files via upload

---
+++ Frontend/src/app/AppSolicitudesCoordi.jsx
@@ -0,0 +1,14 @@
+import '../App.css'
+import Navbar from "../components/NavbarEstudiante";
+import MenuSolicitudesCoordi from "../components/MenuSolicitudesCoordi.jsx";
+
+function AppSolicitudeCoordiF() {
+return (
+    <>
+        <Navbar /> 
+        <MenuSolicitudesCoordi/> 
+    </>
+)
+}
+
+export default AppSolicitudeCoordiF
