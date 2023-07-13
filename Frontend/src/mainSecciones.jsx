2ae18374fab7a392fb0f69630ff92d0e508839df
GelenAmador <98992768+GelenAmador@users.noreply.github.com>
2023-07-12 21:41:38 -0600

    Add files via upload

---
+++ Frontend/src/mainSecciones.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import SeccionesConstruidas from './app/AppSecciones.jsx' 
+import './CssAdministracion.css'
+
+ReactDOM.render(
+    <React.StrictMode>
+        <SeccionesConstruidas/>
+    </React.StrictMode>,
+    document.getElementById('secciones')
+)
