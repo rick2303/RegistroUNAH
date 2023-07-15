70d921d062866f48212f99570a6f11886a8a64c6
Gerardo2811 <gerarmontoya@yahoo.es>
2023-07-14 20:42:41 -0600

    Creacion de secciones y validaciones

--- Backend/src/routes/Secciones.routes.js
+++ Backend/src/routes/Secciones.routes.js
@@ -1,48 +1,17 @@
 import { Router } from 'express';
 import { getConnection, querys, sql } from "../Database";
+import {secciones,traerClases,traerDocentes,crearSeccion,getAulas} from "../controllers/secciones.controllers"
 
 const routerSecciones = Router();
 
-routerSecciones.post('/secciones', async (req, res) => {
-    const { Departamento, CentroRegional } = req.body;
-    const pool = await getConnection();
-    const result = await pool
-        .request()
-        .input('departamento', sql.VarChar, Departamento)
-        .input('centroRegional', sql.VarChar, CentroRegional)
-        .query(`
-            select s.IdClase, s.Edificio, s.Aula, s.CantidadAlumnos, s.HI, s.HF, s.Seccion, s.Periodo, s.Obs, s.Dias, s.Cupos, c.Departamento, s.CentroRegional, (e.Nombre + e.Apellido) as Nombre, c.Nombre AS Clase
-            from [dbo].[secciones] s
-            INNER join [dbo].[empleados] e on e.NumEmpleado = s.IdDocente
-            INNER JOIN [dbo].[clases] c on s.IdClase = c.IdClase
-            where c.Departamento = @departamento and s.CentroRegional = @centroRegional
-        `);
+routerSecciones.post('/secciones',secciones);
 
-    res.json(result.recordset);
-});
+routerSecciones.post('/TraerClases',traerClases);
 
+routerSecciones.post('/TraerDocentes', traerDocentes);
 
-routerSecciones.post('/TraerClases', async (req, res) => {
-    const { Departamento } = req.body;
-    const pool = await getConnection();
-    const result = await pool
-        .request()
-        .input('departamento', sql.VarChar, Departamento)
-        .query(`select * from [dbo].[clases] where Departamento = @departamento`);
+routerSecciones.post('/crearSeccion', crearSeccion);
 
-    res.json(result.recordset);
-});
-
-routerSecciones.post('/TraerDocentes', async (req, res) => {
-    const { Carrera, CentroRegional } = req.body;
-    const pool = await getConnection();
-    const result = await pool
-        .request()
-        .input('carrera', sql.VarChar, Carrera)
-        .input('centroRegional', sql.VarChar, CentroRegional)
-        .query(`SELECT * FROM [dbo].[empleados] WHERE Carrera = @carrera And CentroRegional = @centroRegional`);
-
-    res.json(result.recordset);
-});
+routerSecciones.get('/Aulas/:id', getAulas);
 
 export default routerSecciones;
