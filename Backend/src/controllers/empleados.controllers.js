be553acb74c14773e2d24d2d9d25ff224525dedd
Rick <ricky95hnd@gmail.com>
2023-07-20 22:02:10 -0600

    Agregando ress status(200) en funcion getDocenteById

--- Backend/src/controllers/empleados.controllers.js
+++ Backend/src/controllers/empleados.controllers.js
@@ -32,6 +32,7 @@ export const getDocenteById = async (req, res) => {
         const result = await pool.request()
         .input('DNI', sql.VarChar, DNI)
         .query(queries.get_Docente_By_Id);
+        res.status(200);
         res.json(result.recordset[0]);
     } catch (error) {
         res.status(500);
