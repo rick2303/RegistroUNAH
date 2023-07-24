21db54c51b1069b18b84800a700e1aba08aca542
GelenAmador <98992768+GelenAmador@users.noreply.github.com>
2023-07-23 20:20:43 -0600

    Add files via upload

---
+++ Frontend/src/mainCambioCarrera.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import AppCambioCarrera from './app/AppCambioCarrera.jsx' 
+
+import './CssAdministracion.css'
+
+ReactDOM.render(
+    <React.StrictMode>
+    <AppCambioCarrera />
+
+    </React.StrictMode>,
+    document.getElementById('cambiocarrera')
+)
