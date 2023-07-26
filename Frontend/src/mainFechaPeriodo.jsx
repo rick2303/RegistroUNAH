80b9712dfc2f45bb888eb42ecd68379dcde89ac9
Rick <76531295+rick2303@users.noreply.github.com>
2023-07-26 15:51:39 -0600

    Update mainFechaPeriodo.jsx

--- Frontend/src/mainFechaPeriodo.jsx
+++ Frontend/src/mainFechaPeriodo.jsx
@@ -1,25 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import AppFechaPeriodoSemestral from './app/AppFechaPeriodoSemestral';
 import AppFechaPeriodo from './app/AppFechaPeriodo';
 import {MuiPickersUtilsProvider} from '@material-ui/pickers';
 import DateFnUtils from '@date-io/date-fns';
-import esLocale from "date-fns/locale/es";
-import Navbar from './components/Navbar';
-
-ReactDOM.render(
-	<React.StrictMode>
-   <div className='w-full top-0' style={{ position: 'fixed', zIndex: 999  }}>
-  <Navbar />
-</div>
-
-	
-	<div className='mt-40'>
-	<MuiPickersUtilsProvider utils={DateFnUtils} locale={esLocale}>
-	
+	@@ -20,13 +20,6 @@ ReactDOM.render(
 	<AppFechaPeriodo />
 	</MuiPickersUtilsProvider>	
-		
+
+	</div>
+	<div className='mt-20'>
+	<MuiPickersUtilsProvider utils={DateFnUtils} locale={esLocale}>
+
+	<AppFechaPeriodoSemestral />
+	</MuiPickersUtilsProvider>	
+
 	</div>
 
 	
