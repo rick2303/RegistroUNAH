23a5767fe31d8100e323aba595a200359a57e392
Rick <ricky95hnd@gmail.com>
2023-07-26 10:53:10 -0600

    Agregando routercambiocarrera a app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -46,5 +46,5 @@ console.log(config.port);
 
 app.use(empleadosRoute,loginRoute,perfilEstudiante,perfilEmpleado);
 app.use(usuariosRoute, historialAcademico, routerCancelacionesExep,routerSolicitudes);
-app.use(routerSecciones, routerAdmin, routerEstado);
+app.use(routerSecciones, routerAdmin, routerEstado, routerCambioCarrera);
 
