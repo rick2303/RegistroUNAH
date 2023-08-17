912818488aac536f629d6dbecbc3868fee8b9030
GelenAmador <98992768+GelenAmador@users.noreply.github.com>
2023-08-17 14:25:38 -0600

    Add files via upload

---
+++ Frontend/src/app/AppEstadisticas.jsx
@@ -0,0 +1,15 @@
+import '../App.css'
+import Navbar from "../components/Navbar.jsx";
+import MenuEstadisticas from "../components/menuEstadisticas.jsx";
+
+function AppEstadisticas() {
+
+return (
+<>
+    <Navbar /> 
+    <MenuEstadisticas/> 
+</>
+)
+}
+
+export default AppEstadisticas;
