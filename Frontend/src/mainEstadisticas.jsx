209f9aafce549e3c5a6a8ae4e8a92169e3361a58
GelenAmador <98992768+GelenAmador@users.noreply.github.com>
2023-08-17 14:35:11 -0600

    Add files via upload

---
+++ Frontend/src/mainEstadisticas.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import AppEstadisticas from './app/AppEstadisticas.jsx' 
+
+import './CssAdministracion.css'
+
+ReactDOM.render(
+    <React.StrictMode>
+
+        <AppEstadisticas/>
+
+    </React.StrictMode>,
+    document.getElementById('estadistica')
+)
